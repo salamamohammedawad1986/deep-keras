ce7694b890b7cb57f83fa6950f970c6e8832af4a
salamamohammedawad1986 <sohighsalama@gmail.com>
2023-10-29 14:09:28 +0100

    second commit

--- dl-test.py
+++ dl-test.py
@@ -4,3 +4,12 @@ import numpy as np
 
 
 print('salama')
+
+def odd_even():
+    for num in np.arange(2,10):
+        if(num %2) == 0:
+            print('Even :', num)
+        else:
+            print('Odd:', num)   
+
+odd_even()        
